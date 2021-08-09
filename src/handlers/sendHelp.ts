ce0a8803a4f14325a2e5a976aece4de322ef4834
backmeupplz <backmeupplz@gmail.com>
2021-08-09 10:21:10 -0700

    fix crypto granny bot

--- src/handlers/sendHelp.ts
+++ src/handlers/sendHelp.ts
@@ -1,5 +1,13 @@
 import { Context } from 'telegraf'
 
-export function sendHelp(ctx: Context) {
+export async function sendHelp(ctx: Context) {
+  const startPayload = (ctx as any).startPayload as string
+  if (startPayload && startPayload === 'en') {
+    const user = ctx.dbuser
+    user.language = 'en'
+    await user.save()
+    const anyI18N = ctx.i18n as any
+    anyI18N.locale('en')
+  }
   return ctx.replyWithHTML(ctx.i18n.t('help'))
 }
